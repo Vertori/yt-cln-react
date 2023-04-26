06d21aa7a347deffec48f688c58941c272e34cea
Vertori <115083330+Vertori@users.noreply.github.com>
2023-04-26 22:48:12 +0200

    vite setup, tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1440px',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
