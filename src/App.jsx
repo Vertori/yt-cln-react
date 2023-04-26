bc581b4660e60c75ecb1bd42c51771a42239e278
Vertori <115083330+Vertori@users.noreply.github.com>
2023-04-26 23:27:13 +0200

    website scheme /w routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import {
+  Navbar,
+  Feed,
+  VideoDetail,
+  ChannelDetail,
+  SearchFeed,
+} from "./components";
 
 function App() {
-  
-
   return (
-    <>
-     
-    </>
-  )
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Feed />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="/channel/:id" element={<ChannelDetail />} />
+        <Route path="/search/:searchTerm" element={<SearchFeed />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
