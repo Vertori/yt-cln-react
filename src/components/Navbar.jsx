445264864271cc91b6d3cff2af930218e5d2ca81
Vertori <115083330+Vertori@users.noreply.github.com>
2023-04-27 18:39:02 +0200

    navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import ytLogo from "../assets/yt-icon.png";
+import SearchBar from "./SearchBar";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="w-full h-[90px] bg-[#000] flex justify-between items-center sticky top-0 py-4 px-6">
+      <Link to="/">
+        <img className="h-[45px]" src={ytLogo} alt="YouTube Logo" />
+      </Link>
+      <SearchBar />
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     screens: {
-      sm: '640px',
-      md: '768px',
-      lg: '1024px',
-      xl: '1440px',
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1440px",
     },
     extend: {},
   },
   plugins: [],
-}
-
+};
