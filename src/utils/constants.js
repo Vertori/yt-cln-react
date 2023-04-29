08fd4bea3b87d9f53b4bbe8ebcfafdff0393bb1f
Vertori <115083330+Vertori@users.noreply.github.com>
2023-04-29 20:47:43 +0200

    sidebar component

---
+++ src/utils/constants.js
@@ -0,0 +1,31 @@
+import { AiFillHome } from "react-icons/ai";
+import { FiCode } from "react-icons/fi";
+import { BsMusicNoteBeamed } from "react-icons/bs";
+import { IoMdSchool } from "react-icons/io";
+import { BiPodcast } from "react-icons/bi";
+import { BiCameraMovie } from "react-icons/bi";
+import { FaGamepad } from "react-icons/fa";
+import { MdLiveTv } from "react-icons/md";
+import { FaFootballBall } from "react-icons/fa";
+import { TbHanger } from "react-icons/tb";
+import { MdFace3 } from "react-icons/md";
+import { MdTheaterComedy } from "react-icons/md";
+import { TbBarbell } from "react-icons/tb";
+import { FaBitcoin } from "react-icons/fa";
+
+export const categories = [
+  { name: "New", icon: AiFillHome },
+  { name: "Coding", icon: FiCode },
+  { name: "Music", icon: BsMusicNoteBeamed },
+  { name: "Education", icon: IoMdSchool },
+  { name: "Podcast", icon: BiPodcast },
+  { name: "Movie", icon: BiCameraMovie },
+  { name: "Gaming", icon: FaGamepad },
+  { name: "Live", icon: MdLiveTv },
+  { name: "Sport", icon: FaFootballBall },
+  { name: "Fashion", icon: TbHanger },
+  { name: "Beauty", icon: MdFace3 },
+  { name: "Comedy", icon: MdTheaterComedy },
+  { name: "Gym", icon: TbBarbell },
+  { name: "Crypto", icon: FaBitcoin },
+];
