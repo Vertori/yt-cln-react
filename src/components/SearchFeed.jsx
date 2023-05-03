f774ec69c2ac8e05ed44551ef8bb6ef8c8e719a5
Vertori <115083330+Vertori@users.noreply.github.com>
2023-05-03 10:30:26 +0200

    search feed component

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Videos } from "./";
 
 const SearchFeed = () => {
-  return (
-    <div>SearchFeed</div>
-  )
-}
+  const { searchTerm } = useParams();
+  const [videos, setVideos] = useState([]);
 
-export default SearchFeed
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then((data) =>
+      setVideos(data.items)
+    );
+  }, [searchTerm]);
+
+  return (
+    <div className="h-[90vh] p-8 overflow-y-auto flex-2">
+      <div className="text-3xl mb-8 text-white font-bold">
+        Search Results For <span className="text-[#F31503]">{searchTerm}</span>{" "}
+        videos
+      </div>
+      <Videos videos={videos} />
+    </div>
+  );
+};
+
+export default SearchFeed;
